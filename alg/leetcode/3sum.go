7e8184713aea18a05262fea84b0c811a45d74cf7
brofu <changfu.shao@gmail.com>
2021-11-23 17:16:32 +0800

    add 3sum

--- alg/leetcode/3sum.go
+++ alg/leetcode/3sum.go
@@ -1,55 +1,153 @@
 package leetcode
 
+import (
+	"fmt"
+	"sort"
+)
+
+// Key Points
+// 2 pointer
+// sort and remove duplicate in loop control
+
 func threeSum(nums []int) [][]int {
 
-	result := [][]int{}
+	var (
+		result = [][]int{}
+		n      = len(nums)
+	)
+
+	if n < 3 {
+		return result
+	}
+
+	sort.Ints(nums)
+
+	for index := 0; index < n-2; index++ {
+
+		if index > 0 && nums[index] == nums[index-1] {
+			continue
+		}
+
+		var (
+			target = -nums[index]
+			left   = index + 1
+			right  = n - 1
+		)
+
+		for left < right {
+
+			sum := nums[left] + nums[right]
+			if sum == target {
+				result = append(result, []int{-target, nums[left], nums[right]}) // collect result
+				left++
+				right--
+				for left < right && (nums[left] == nums[left-1]) { // adjust left pointer
+					left++
+				}
+				for left < right && (nums[right] == nums[right+1]) { // adjust right pointer
+					right--
+				}
+			} else if sum < target {
+				left++
+				for nums[left] == nums[left-1] { // adjust left pointer
+					left++
+				}
+			} else {
+				right--
+				for nums[right] == nums[right+1] { // adjust right pointer
+					right--
+				}
+			}
+
+		}
+	}
+
+	return result
+}
+
+// deprecated version
+// exceed running time
+// Key point: remove duplicate results
+
+func threeSumDeprecated(nums []int) [][]int {
+
+	var (
+		result      = [][]int{}
+		finalResult = [][]int{}
+		hit         = []map[int]int{}
+	)
 
 	if len(nums) < 3 {
 		return result
 	}
 
-	hit := [][]int{}
 	for index, num := range nums {
-
 		target := 0 - num
-
-		tempResult := twoSumValueWithOrder(nums, target, index)
-
+		tempResult := twoSumValue(nums, target, index)
 		result = append(result, tempResult...)
 	}
 
 	for _, pair := range result {
-		x, y := pair[0], pair[1]
-		tempResult := make([]int, 3)
 
-		num := 0 - (x + y)
+		tr := map[int]int{}
 
-		if num <= x {
-			tempResult[0] = num
-			tempResult[1] = x
-			tempResult[2] = y
+		for _, v := range pair {
+			MapAdd(tr, v)
 		}
+		MapAdd(tr, 0-(pair[0]+pair[1]))
 
-		if num > y {
-			tempResult[0] = x
-			tempResult[1] = y
-			tempResult[2] = num
-		}
+		fmt.Println("flag", tr)
 
-		if x < num && num <= y {
-			tempResult[0] = x
-			tempResult[1] = num
-			tempResult[2] = y
-		}
-
-		for _, r := range hit {
-			if !compareTwoList(tempResult, r) {
-				hit = append(hit, tempResult)
+		if len(hit) == 0 {
+			hit = append(hit, tr)
+		} else {
+			exists := false
+			for _, r := range hit {
+				if compareTwoMap(tr, r) {
+					exists = true
+				}
+			}
+			if !exists {
+				hit = append(hit, tr)
 			}
 		}
 	}
 
-	return hit
+	for _, v := range hit {
+		t := make([]int, 3)
+		index := 0
+		for k, count := range v {
+			for idx := 0; idx < count; idx++ {
+				t[index] = k
+				index++
+			}
+		}
+		finalResult = append(finalResult, t)
+	}
+
+	return finalResult
+}
+
+func MapAdd(data map[int]int, ele int) {
+	if _, exists := data[ele]; exists {
+		data[ele]++
+	} else {
+		data[ele] = 1
+	}
+}
+
+func compareTwoMap(nums1, nums2 map[int]int) (equal bool) {
+	if len(nums1) != len(nums2) {
+		return false
+	}
+
+	for k := range nums1 {
+		if _, exists := nums2[k]; !exists {
+			return false
+		}
+	}
+
+	return true
 }
 
 func compareTwoList(nums1, nums2 []int) bool {
@@ -58,6 +156,30 @@ func compareTwoList(nums1, nums2 []int) bool {
 	}
 
 	return nums1[0] == nums2[0] && nums1[1] == nums2[1]
+}
+
+func twoSumValue(nums []int, target int, index int) [][]int {
+	dataMap := make(map[int]bool)
+	result := [][]int{}
+
+	for idx, num := range nums {
+
+		if idx == index {
+			continue
+		}
+
+		nextOne := target - num
+
+		_, exists := dataMap[nextOne]
+
+		if exists {
+			result = append(result, []int{num, nextOne})
+		} else {
+			dataMap[num] = true
+		}
+	}
+
+	return result
 }
 
 func twoSumValueWithOrder(nums []int, target int, index int) [][]int {
--- alg/leetcode/3sum_test.go
+++ alg/leetcode/3sum_test.go
@@ -3,11 +3,18 @@ package leetcode
 import "testing"
 
 func TestTwoSumWithSkip(t *testing.T) {
-
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	result := twoSumValueWithOrder(nums, 1, 0)
+	t.Log(result)
 }
 
 func TestThreeSum(t *testing.T) {
 	nums := []int{-1, 0, 1, 2, -1, -4}
+	result := threeSum(nums)
+	t.Log(result)
+}
+func TestThreeSumV1(t *testing.T) {
+	nums := []int{0, 0, 0}
 	result := threeSum(nums)
 	t.Log(result)
 }
