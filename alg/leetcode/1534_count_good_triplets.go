b7b5d7366f8c24c373a8c95f2b85d30cd45a0246
brofu <changfu.shao@gmail.com>
2021-12-02 00:06:57 +0800

    get the best solution of count good triplets

--- alg/leetcode/1534_count_good_triplets.go
+++ alg/leetcode/1534_count_good_triplets.go
@@ -27,6 +27,8 @@ func AbsInt(x int) int {
 	return x
 }
 
+// 232ms 5.88%
+// 6.9mb 11.76%
 func countGoodTripletsV2(arr []int, a int, b int, c int) int {
 	var (
 		count        int
@@ -34,8 +36,8 @@ func countGoodTripletsV2(arr []int, a int, b int, c int) int {
 		temp1, temp2 [][]int
 	)
 
-	for i := 0; i < length-3; i++ {
-		for j := i + 1; j < length-2; j++ {
+	for i := 0; i < length-2; i++ {
+		for j := i + 1; j < length-1; j++ {
 			if AbsInt(arr[i]-arr[j]) <= a {
 				temp1 = append(temp1, []int{i, j})
 			}
@@ -43,8 +45,8 @@ func countGoodTripletsV2(arr []int, a int, b int, c int) int {
 	}
 
 	for k := length - 1; k > 1; k-- {
-		for j := k - 1; j >= 0; j-- {
-			if AbsInt(arr[k]-arr[j]) <= b {
+		for j := k - 1; j > 0; j-- {
+			if AbsInt(arr[j]-arr[k]) <= b {
 				temp2 = append(temp2, []int{j, k})
 			}
 		}
@@ -61,5 +63,60 @@ func countGoodTripletsV2(arr []int, a int, b int, c int) int {
 			}
 		}
 	}
+	return count
+}
+
+// 8ms 70.59%
+// 6.6mb 11.76%
+func countGoodTripletsV3(arr []int, a int, b int, c int) int {
+	var (
+		count  int
+		length = len(arr)
+		temp1  [][]int
+	)
+
+	for i := 0; i < length-2; i++ {
+		for j := i + 1; j < length-1; j++ {
+			if AbsInt(arr[i]-arr[j]) <= a {
+				temp1 = append(temp1, []int{i, j})
+			}
+		}
+	}
+
+	for _, temp := range temp1 {
+		i := temp[0]
+		j := temp[1]
+		for k := j + 1; k < length; k++ {
+			if AbsInt(arr[j]-arr[k]) <= b && AbsInt(arr[i]-arr[k]) <= c {
+				count += 1
+			}
+		}
+	}
+
+	return count
+}
+
+// 4ms 100%
+// 2.3mb 100%
+// compare v3:
+// 2 cycle of O(n*2) --> 1 cycle
+func countGoodTripletsV4(arr []int, a int, b int, c int) int {
+	var (
+		count  int
+		length = len(arr)
+	)
+
+	for i := 0; i < length-2; i++ {
+		for j := i + 1; j < length-1; j++ {
+			if AbsInt(arr[i]-arr[j]) <= a {
+				for k := j + 1; k < length; k++ {
+					if AbsInt(arr[j]-arr[k]) <= b && AbsInt(arr[i]-arr[k]) <= c {
+						count += 1
+					}
+				}
+			}
+		}
+	}
+
 	return count
 }
--- alg/leetcode/1534_count_good_triplets_test.go
+++ alg/leetcode/1534_count_good_triplets_test.go
@@ -8,12 +8,18 @@ import (
 
 func TestCountGoodTriplets(t *testing.T) {
 
-	arr := []int{7, 3, 7, 3, 12, 1, 12, 2, 3}
-	a := 5
-	b := 8
-	c := 1
+	var (
+		arr    = []int{7, 3, 7, 3, 12, 1, 12, 2, 3}
+		a      = 5
+		b      = 8
+		c      = 1
+		result int
+	)
 
-	result := countGoodTripletsV2(arr, a, b, c)
-
+	result = countGoodTripletsV2(arr, a, b, c)
 	assert.Equal(t, 12, result)
+
+	result = countGoodTripletsV3(arr, a, b, c)
+	assert.Equal(t, 12, result)
+
 }
